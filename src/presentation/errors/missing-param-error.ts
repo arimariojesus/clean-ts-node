9434503c83fe7a13729387d215b5467713b0bb39
arimariojesus <arimario.jesus@hotmail.com>
2022-03-06 19:14:45 -0300

    style: message spelling

--- src/presentation/errors/missing-param-error.ts
+++ src/presentation/errors/missing-param-error.ts
@@ -1,6 +1,6 @@
 export class MissingParamError extends Error {
   constructor (paramName: string) {
-    super(`Misssin param: ${paramName}`)
+    super(`Missing param: ${paramName}`)
     this.name = 'MissingParamError'
   }
 }
