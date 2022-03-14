1bde2fdfd820746b3a913fa23982b74d205ca693
arimariojesus <arimario.jesus@hotmail.com>
2022-03-14 00:56:24 -0300

    feat: ensure requests return json as default content type

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -2,9 +2,9 @@ import request from 'supertest'
 import app from '../config/app'
 
 describe('CORS Middleware', () => {
-  test('Should parse body as json', async () => {
+  test('Should enable CORS', async () => {
     app.get('/test_cors', (req, res) => {
-      res.send(req.body)
+      res.send()
     })
     await request(app)
       .get('/test_cors')
