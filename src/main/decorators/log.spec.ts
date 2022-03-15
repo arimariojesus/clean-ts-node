a1996aff6c1d574e7a678bc0108816af892bb244
arimariojesus <arimario.jesus@hotmail.com>
2022-03-15 01:23:58 -0300

    fix: back call controller handle test

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -32,6 +32,21 @@ const makeSut = (): SutTypes => {
 
 describe('LogController Decorator', () => {
   it('Should call controller handle', async () => {
+    const { sut, controllerStub } = makeSut()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+
+  it('Should return the same result of the controller', async () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: {
