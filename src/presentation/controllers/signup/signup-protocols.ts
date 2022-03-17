a28f6d26b573224e2e332e584416dc948bffce0f
arimariojesus <arimario.jesus@hotmail.com>
2022-03-17 01:31:31 -0300

    feat: ensure SignUpController calls Validation with correct value

--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -1,4 +1,5 @@
 export * from '../../protocols'
 export * from '../../protocols/email-validator'
+export * from '../../helpers/validators/validaton'
 export * from '../../../domain/usecases/add-account'
 export * from '../../../domain/models/account'
