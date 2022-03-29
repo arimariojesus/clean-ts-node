6495de2e4ee86e7fb4e72fa61b49d5d95f7f99d2
arimariojesus <arimario.jesus@hotmail.com>
2022-03-29 02:21:49 -0300

    test: ensure AccountMongoRepository updates the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -56,4 +56,19 @@ describe('Account Mongo Repository', () => {
     const account = await sut.loadByEmail('any_email@mail.com')
     expect(account).toBeFalsy()
   })
+
+  test('Should update the account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut()
+    const res = await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    const { insertedId: _id } = res
+    const idAsString = _id.toHexString()
+    await sut.updateAccessToken(idAsString, 'any_token')
+    const account = await accountCollection.findOne({ _id })
+    expect(account).toBeTruthy()
+    expect(account.accessToken).toBe('any_token')
+  })
 })
