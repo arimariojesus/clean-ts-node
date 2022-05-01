0537cc3602fc22a91006fa9b142ad3787fc02716
arimariojesus <arimario.jesus@hotmail.com>
2022-05-01 19:59:26 -0300

    feat: ensure AddSurveyController returns 204 on success

--- src/presentation/helpers/http/http-helpers.ts
+++ src/presentation/helpers/http/http-helpers.ts
@@ -35,3 +35,10 @@ export const ok = (data: any): HttpResponse => {
     body: data
   }
 }
+
+export const noContent = (): HttpResponse => {
+  return {
+    statusCode: 204,
+    body: null
+  }
+}
