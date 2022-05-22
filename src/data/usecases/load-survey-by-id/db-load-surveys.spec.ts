2735dd10868899cff0d4ff2171ae06f8152a6f11
arimariojesus <arimario.jesus@hotmail.com>
2022-05-22 00:01:45 -0300

    test: ensure DbLoadSurveyById throes if LoadSurveyById throws

--- src/data/usecases/load-survey-by-id/db-load-surveys.spec.ts
+++ src/data/usecases/load-survey-by-id/db-load-surveys.spec.ts
@@ -57,4 +57,11 @@ describe('DbLoadSurveyById', () => {
     const survey = await sut.loadById('any_id')
     expect(survey).toEqual(makeFakeSurvey())
   })
+
+  test('Should throw if LoadSurveysByIdRepository throws', async () => {
+    const { sut, loadSurveyByIdRepositoryStub } = makeSut()
+    jest.spyOn(loadSurveyByIdRepositoryStub, 'loadById').mockReturnValueOnce(Promise.reject(new Error()))
+    const promise = sut.loadById('any_id')
+    await expect(promise).rejects.toThrow()
+  })
 })
