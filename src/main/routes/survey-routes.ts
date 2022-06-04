c38693c25d0bf031d8611a2830a116b03aeec2fc
arimariojesus <arimario.jesus@hotmail.com>
2022-06-03 23:47:50 -0300

    feat: ensure save survey result route returns 403 if no token is provided

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
-import { makeLoadSurveysController } from '../factories/controllers/survey/load-surveys/add-surveys-controller-factory'
+import { makeLoadSurveysController } from '../factories/controllers/survey/load-surveys/load-surveys-controller-factory'
 import { adminAuth } from '../middlewares/admin-auth'
 import { auth } from '../middlewares/auth'
 
