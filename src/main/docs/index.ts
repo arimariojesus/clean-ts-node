4bc3c72162e340414bc8531c17a08c80a1ef4e80
arimariojesus <arimario.jesus@hotmail.com>
2022-06-19 16:47:58 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,10 @@
+export default {
+  openapi: '3.0.3',
+  info: {
+    title: 'Clean Node API',
+    description: 'API para realização de enquetes entre programadores',
+    version: '1.0.0',
+    author: 'Arimário Jesus <arimario.jesus@hotmail.com> (https://github.com/arimariojesus)',
+    termsOfService: '#'
+  }
+}
