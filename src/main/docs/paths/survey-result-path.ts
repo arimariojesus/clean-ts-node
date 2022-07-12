9dfcf93e95fcfc208c1cb05a9ee884514ef98116
arimariojesus <arimario.jesus@hotmail.com>
2022-07-11 21:27:46 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -8,6 +8,7 @@ export const surveyResultPath = {
     parameters: [{
       in: 'path',
       name: 'surveyId',
+      description: 'ID da enquete a ser responsidade',
       required: true,
       schema: {
         type: 'string'
@@ -22,6 +23,38 @@ export const surveyResultPath = {
         }
       }
     },
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/survey-result'
+            }
+          }
+        }
+      },
+      403: { $ref: '#/components/forbidden' },
+      404: { $ref: '#/components/not-found' },
+      500: { $ref: '#/components/server-error' }
+    }
+  },
+  get: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Enquete'],
+    summary: 'API para consultar o resultado de uma enquete',
+    description: 'Essa rota só pode ser executada por **usuários autenticados**',
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      description: 'ID da enquete a ser responsidade',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
     responses: {
       200: {
         description: 'Sucesso',
