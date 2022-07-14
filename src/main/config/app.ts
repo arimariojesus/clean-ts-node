8cbf5ea4aff41e8fe96620cf67da983d94e6c838
arimariojesus <arimario.jesus@hotmail.com>
2022-07-14 02:04:52 -0300

    feat: add route to server static files

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
+import setupStaticFiles from './static-files'
 import setupMiddlewares from './middlewares'
 import setupSwagger from './config-swagger'
 import setupRoutes from './routes'
 
 const app = express()
+setupStaticFiles(app)
 setupSwagger(app)
 setupMiddlewares(app)
 setupRoutes(app)
