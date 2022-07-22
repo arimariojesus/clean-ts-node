11fcb2d6b5235642ccc9069bc65fee640d2c82a9
arimariojesus <arimario.jesus@hotmail.com>
2022-07-21 21:49:59 -0300

    feat: add status code error on exceptions

--- src/main/config/apollo-server.ts
+++ src/main/config/apollo-server.ts
@@ -2,8 +2,35 @@ import { ApolloServer } from 'apollo-server-express'
 
 import typeDefs from '@/main/graphql/type-defs'
 import resolvers from '@/main/graphql/resolvers'
+import { GraphQLError } from 'graphql'
+
+const checkError = (error: GraphQLError, errorName: string): boolean =>
+  [error.name, error.originalError.name].includes(errorName)
+
+const handleErrors = (response: any, errors: readonly GraphQLError[]): void => {
+  errors?.forEach(error => {
+    response.data = undefined
+
+    if (checkError(error, 'UserInputError')) {
+      response.http.status = 400
+    } else if (checkError(error, 'AuthenticationError')) {
+      response.http.status = 401
+    } else if (checkError(error, 'ForbiddenError')) {
+      response.http.status = 403
+    } else {
+      response.http.status = 500
+    }
+  })
+}
 
 export const setupApolloServer = (): ApolloServer => new ApolloServer({
   resolvers,
-  typeDefs
+  typeDefs,
+  plugins: [{
+    requestDidStart: async () => {
+      return {
+        willSendResponse: async ({ response, errors }) => handleErrors(response, errors)
+      }
+    }
+  }]
 })
