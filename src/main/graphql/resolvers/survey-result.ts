cff75638cca01344bcbb226356836769e91c93d2
arimariojesus <arimario.jesus@hotmail.com>
2022-07-22 00:32:01 -0300

    feat: add SurveyResult query and mutation

---
+++ src/main/graphql/resolvers/survey-result.ts
@@ -0,0 +1,17 @@
+import { adaptResolver } from '@/main/adapters'
+import { makeLoadSurveyResultController } from '@/main/factories/controllers/survey-result/load-survey-result/load-survey-result-controller-factory'
+import { makeSaveSurveyResultController } from '@/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory'
+
+export default {
+  Query: {
+    async surveyResult (parent: any, args: any) {
+      return adaptResolver(makeLoadSurveyResultController(), args)
+    }
+  },
+
+  Mutation: {
+    async saveSurveyResult (parent: any, args: any) {
+      return adaptResolver(makeSaveSurveyResultController(), args)
+    }
+  }
+}
