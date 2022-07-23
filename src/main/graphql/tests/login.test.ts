97bd234c4418f349684f3585f2c999d67fc55144
arimariojesus <arimario.jesus@hotmail.com>
2022-07-22 23:24:28 -0300

    test: ensure SignUp Mutation returns an account on valid data

--- src/main/graphql/tests/login.test.ts
+++ src/main/graphql/tests/login.test.ts
@@ -27,34 +27,55 @@ describe('Login GraphQL', () => {
   const name = 'John Doe'
   const email = 'john.doe@mail.com'
   const password = '123'
-  const query = `query {
-    login (email: "${email}", password: "${password}") {
-      accessToken
-      name
-    }
-  }`
 
-  test('Should return an Account on valid credentials', async () => {
-    const hashedPassword = await hash(password, 12)
-    await accountCollection.insertOne({
-      name,
-      email,
-      password: hashedPassword
+  describe('Login Query', () => {
+    const query = `query {
+      login (email: "${email}", password: "${password}") {
+        accessToken
+        name
+      }
+    }`
+
+    test('Should return an Account on valid credentials', async () => {
+      const hashedPassword = await hash(password, 12)
+      await accountCollection.insertOne({
+        name,
+        email,
+        password: hashedPassword
+      })
+      const res = await request(app)
+        .post('/graphql')
+        .send({ query })
+      expect(res.status).toBe(200)
+      expect(res.body.data.login.accessToken).toBeTruthy()
+      expect(res.body.data.login.name).toBe(name)
     })
-    const res = await request(app)
-      .post('/graphql')
-      .send({ query })
-    expect(res.status).toBe(200)
-    expect(res.body.data.login.accessToken).toBeTruthy()
-    expect(res.body.data.login.name).toBe(name)
+
+    test('Should return UnauthorizedError on invalid credentials', async () => {
+      const res = await request(app)
+        .post('/graphql')
+        .send({ query })
+      expect(res.status).toBe(401)
+      expect(res.body.data).toBeFalsy()
+      expect(res.body.errors[0].message).toBe('Unauthorized')
+    })
   })
 
-  test('Should return UnauthorizedError on invalid credentials', async () => {
-    const res = await request(app)
-      .post('/graphql')
-      .send({ query })
-    expect(res.status).toBe(401)
-    expect(res.body.data).toBeFalsy()
-    expect(res.body.errors[0].message).toBe('Unauthorized')
+  describe('SignUp Mutation', () => {
+    const query = `mutation {
+      signUp (name: "${name}", email: "${email}", password: "${password}", passwordConfirmation: "${password}") {
+        accessToken
+        name
+      }
+    }`
+
+    test('Should return an Account on valid data', async () => {
+      const res = await request(app)
+        .post('/graphql')
+        .send({ query })
+      expect(res.status).toBe(200)
+      expect(res.body.data.signUp.accessToken).toBeTruthy()
+      expect(res.body.data.signUp.name).toBe(name)
+    })
   })
 })
